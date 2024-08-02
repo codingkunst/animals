ca9850c19a11fbc172115e4641ccf761aad997ac
codingkunst <ryoo6931@gmail.com>
2024-08-02 15:58:31 +0900

    [FEAT] App 인스턴스 생성

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-const API_URL = "https://animal-api-two.vercel.app/";
+import App from "./App";
 
-const $content = document.querySelector("div.content");
-let template = [];
-const getData = async () => {
-  let res = await fetch(API_URL);
-  try {
-    if (res) {
-      let data = await res.json();
-      console.log(data);
-      data.photos.forEach((item) => {
-        console.log(item);
-        template += `<img src=${item.url}></img>`;
-      });
-      $content.innerHTML = template;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
+const $app = document.getElementById('app');
 
-getData();
+new App($app);
